159ca501745c3480ec5c8e5cea2076984796f2bd
Benjamin Lemin <benjamin.lemin@gmail.com>
2020-10-12 22:05:38 +0200

    Fractal : Mandelbrot set implemented; the zoom in function would be nice

--- assets/js/coding_challenges/fractal_set/engine.js
+++ assets/js/coding_challenges/fractal_set/engine.js
@@ -1,2 +1,6 @@
 export const CANVAS_WIDTH = 485;
 export const CANVAS_HEIGHT = 485;
+
+export const SET_MIN_VAL = -1.0222;
+export const SET_MAX_VAL = 0.0123;
+export const SET_MAX_ITERATION = 150;
--- assets/js/coding_challenges/fractal_set/sketch.js
+++ assets/js/coding_challenges/fractal_set/sketch.js
@@ -7,10 +7,49 @@ function Sketch(p5) {
 
   p5.setup = function () {
     playfield = new Playfield(p5, engine.CANVAS_WIDTH, engine.CANVAS_HEIGHT);
+    p5.pixelDensity(1);
   };
 
   p5.draw = function () {
-    playfield.show();
+    p5.loadPixels();
+
+    for (let x = 0; x < playfield.width; x++) {
+      for (let y = 0; y < playfield.height; y++) {
+
+        let ca = p5.map(x, 0, playfield.width, engine.SET_MIN_VAL, engine.SET_MAX_VAL);
+        let cb = p5.map(y, 0, playfield.height, engine.SET_MIN_VAL, engine.SET_MAX_VAL);
+
+        let a = ca;
+        let b = cb;
+        let n;
+
+        for (n = 0; n < engine.SET_MAX_ITERATION; n++) {
+          let real = (a * a) - (b * b);
+          let imaginary = 2 * a * b;
+
+          a = real + ca;
+          b = imaginary + cb;
+
+          if (Math.abs(a + b) > 16) {
+            break;
+          }
+        }
+
+        let bright = p5.map(n, 0, engine.SET_MAX_ITERATION, 0, 1);
+        bright = p5.map(Math.sqrt(bright), 0, 1, 0, 255);
+        if (n === engine.SET_MAX_ITERATION) {
+          bright = 0;
+        }
+
+        const pix = (x + y * playfield.width) * 4;
+        p5.pixels[pix] = bright;
+        p5.pixels[pix + 1] = bright;
+        p5.pixels[pix + 2] = bright;
+        p5.pixels[pix + 3] = 255;
+      }
+    }
+
+    p5.updatePixels();
   };
 }
 
